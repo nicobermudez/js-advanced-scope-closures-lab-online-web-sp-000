dab571b0bcdb271b63f2e2ea1d9080d574dd0982
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 18:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function produceDrivingRange(range) {
     return function(string1, string2) {
       let start = parseInt(string1)
       let finish = parseInt(string2)
+      let diff = Math.abs(start-finish)
       if(out of range) {
         return `${} blocks out of range`
       } else {
