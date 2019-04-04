4c97d841f7cd879f7c44a8b6e99b0f8a80ddeb63
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 18:42:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range) {
     return function(string1, string2) {
-      let s1 = string1.slice 
+      let s1 = string1.slice
       let s2 = string2.slice()
       if(out of range) {
         return `${} blocks out of range`
